3f42826203bad5bd2311ecbec716ae37d65c028e
Patrick Teufert <patrick.teufert@gmail.com>
2017-07-01 14:38:45 +0200

    adds linux support

--- src/explorerHandler.ts
+++ src/explorerHandler.ts
@@ -4,8 +4,31 @@ import { Utilities } from './utilities';
 export class FileExplorerHandler {
 
 	public static openFolder(uri: string): void {
+		switch (process.platform) {
+			case 'linux':
+				this.openLinux(uri);
+				break;
+			case 'darwin':
+				this.openMac(uri);
+				break;
+			//case 'win32':
+		}
+	}
+
+	private static createPath(uri: string): string {
 		const parts = Utilities.splitKeepDelimeter(uri, '/');
 		const argument = Utilities.combineToPathWithoutFileName(parts);
+		return argument;
+	}
+
+	private static openLinux(uri: string): void {
+		const argument = this.createPath(uri);
+		const command = 'xdg-open  ' + argument + '/';
+		child_process.exec(command);
+	}
+
+	private static openMac(uri: string): void {
+		const argument = this.createPath(uri);
 
 		const command = 'open ' + argument + '/';
 		child_process.exec(command);
